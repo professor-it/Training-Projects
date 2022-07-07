d105a7e7968998721dd56e880dc053bcfc81b4ed
Dima Nikolaenko <dima.nikolaenko.ua@gmail.com>
2022-07-07 20:50:56 +0300

    Chess-base

---
+++ chess-base/src/components/BoardComponent.tsx
@@ -0,0 +1,48 @@
+import React, {FC, useState} from 'react';
+import {Board} from "../models/Board";
+import CellComponent from "./CellComponent";
+import {Cell} from "../models/Cell";
+
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if(cell.figure) {
+      setSelectedCell(cell)
+    }
+  }
+
+  function highlightCells() {
+    board.highlightCells(selectedCell)
+    updateBoard()
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard()
+    setBoard(newBoard)
+  }
+
+  return (
+    <div className='board'>
+      {board.cells.map((row, index) =>
+        <React.Fragment key={index}>
+          {row.map(cell =>
+            <CellComponent
+              click={click}
+              cell={cell}
+              key={cell.id}
+              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+            />
+          )}
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
+
+export default BoardComponent;
