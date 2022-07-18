c2bf73750ade0ce841aa209ed2560a84f6eb3f6b
Dima Nikolaenko <dima.nikolaenko.ua@gmail.com>
2022-07-18 22:50:53 +0300

    Post

--- sanity-medium-build/pages/index.tsx
+++ sanity-medium-build/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home({posts}: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Medium Blog [2.0]</title>
+        <title>Medium [2.0]</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
