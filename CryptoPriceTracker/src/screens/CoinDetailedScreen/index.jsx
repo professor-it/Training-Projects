3ea8cd1d7067116c36b54105c4d287d5a4d5c961
Nikolaenko <dima.nikolaenko.ua@gmail.com>
2022-08-01 19:05:45 +0300

    coindetailedscreen

--- CryptoPriceTracker/src/screens/CoinDetailedScreen/index.jsx
+++ CryptoPriceTracker/src/screens/CoinDetailedScreen/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Image} from 'react-native'
+import {Ionicons, EvilIcons} from '@expo/vector-icons';
+import Coin from '../../../assets/data/crypto.json'
 
 const CoinDetailedScreen = () => {
+	const {image: {small}, name, symbol, market_data: {market_cap_rank}} = Coin;
 	return (
-		<View>
-			<Text style={{color: 'white'}}>Detailed Screen</Text>
+		<View style={{flexDirection: 'row'}}>
+			<Ionicons name='chevron-back-sharp' size={30} color='white'/>
+			<Image source={{uri: small}} style={{width: 25, height: 25}}/>
+			<Text style={{color: 'white'}}>{symbol.toUpperCase()}</Text>
+			<Text style={{color: 'white'}}>#{market_cap_rank}</Text>
+			<EvilIcons name='user' size={30} color='white'/>
 		</View>
 	)
 }
