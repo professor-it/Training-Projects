edc3e9b71a199f169408d6a278a02d9431f676e9
Dima Nikolaenko <dima.nikolaenko.ua@gmail.com>
2022-08-03 20:02:57 +0300

    CoinDetailedHeader

--- CryptoPriceTracker/src/screens/CoinDetailedScreen/index.jsx
+++ CryptoPriceTracker/src/screens/CoinDetailedScreen/index.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
-import {View, Text, Image} from 'react-native'
-import {Ionicons, EvilIcons} from '@expo/vector-icons';
+import {View, Text} from 'react-native'
 import Coin from '../../../assets/data/crypto.json'
+import CoinDetailedHeader from './components/CoinDetailedHeader'
+import styles from './styles'
+import {AntDesign} from '@expo/vector-icons'
 
 const CoinDetailedScreen = () => {
-	const {image: {small}, name, symbol, market_data: {market_cap_rank}} = Coin;
+	const {image: {small}, name, symbol, market_data: {market_cap_rank, current_price, price_change_percentage_24h}} = Coin;
+
+	const percentageColor = price_change_percentage_24h < 0 ? "#ea3943" : '#16c784'
+
 	return (
-		<View style={{flexDirection: 'row'}}>
-			<Ionicons name='chevron-back-sharp' size={30} color='white'/>
-			<Image source={{uri: small}} style={{width: 25, height: 25}}/>
-			<Text style={{color: 'white'}}>{symbol.toUpperCase()}</Text>
-			<Text style={{color: 'white'}}>#{market_cap_rank}</Text>
-			<EvilIcons name='user' size={30} color='white'/>
+		<View style={{paddingHorizontal: 10}}>
+			<CoinDetailedHeader image={small} name={name} symbol={symbol} marketCapRank={market_cap_rank}/>
+			<View style={styles.priceContainer}>
+				<View>
+					<Text style={styles.name}>{name}</Text>
+					<Text style={styles.currentPrice}>${current_price.usd}</Text>
+				</View>
+				<View style={{backgroundColor: {percentageColor}, paddingHorizontal: 3, paddingVertical: 8, borderRadius: 5, flexDirection: 'row'}}>
+					<AntDesign name={price_change_percentage_24h < 0 ? 'caretdown' : 'caretup'}
+					           size={12}
+					           color={'white'}
+					           style={{alignSelf: 'center', marginRight: 5}}
+					/>
+					<Text style={styles.priceChange}>
+						{price_change_percentage_24h.toFixed(2)}%
+					</Text>
+				</View>
+			</View>
 		</View>
 	)
 }
