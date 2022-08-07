ae6a3c900c668f822adab4c7a72ed29c5b0a63df
Dima Nikolaenko <dima.nikolaenko.ua@gmail.com>
2022-08-07 20:06:43 +0300

    Fetching Data with Axios

--- CryptoPriceTracker/src/navigation/index.jsx
+++ CryptoPriceTracker/src/navigation/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import HomeScreen from '../screens/HomeScreen'
 import CoinDetailedScreen from '../screens/CoinDetailedScreen'
+import BottomTabNavigator from './BottomTabNavigator'
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
 	return (
 		<Stack.Navigator
-			initialRouteName="Home"
+			initialRouteName="Root"
 			screenOptions={{headerShown: false}}
 		>
-			<Stack.Screen name={"Home"} component={HomeScreen}/>
+			<Stack.Screen name={"Root"} component={BottomTabNavigator}/>
 			<Stack.Screen name={"CoinDetailed"} component={CoinDetailedScreen}/>
 		</Stack.Navigator>
 	)
